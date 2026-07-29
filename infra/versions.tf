49a9d11a4200625a63290f7c3bce6925b97a6f1c
T Sai Sreeprada <sreepradatummalapalli@gmail.com>
2026-07-29 19:53:47 +0530

    chore: add Lambda deployment packaging

--- infra/versions.tf
+++ infra/versions.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = "~> 1.15"
 
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.8"
+    }
+
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.55"
